5085141812bd954680adc1bad5a4066267093923
ScreenshotRU <screenshot@inbox.ru>
2021-11-25 03:10:05 +0700

    Add some framework logic

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,3 +1,19 @@
 import { ExcelComponent } from '@core/ExcelComponent';
 
-export class Header extends ExcelComponent {}
+export class Header extends ExcelComponent {
+  static className = 'excel__header';
+
+  toHTML() {
+    return `
+    <input type="text" class="input" id="1" value="New table" />
+    <div>
+        <div class="button">
+            <span class="material-icons">delete</span>
+        </div>
+        <div class="button">
+            <span class="material-icons">exit_to_app</span>
+        </div>
+    </div>
+    `;
+  }
+}
