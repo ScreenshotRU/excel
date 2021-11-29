bdbb9dbf9d9d0ccac46938e445042d4389c14bbd
ScreenshotRU <screenshot@inbox.ru>
2021-11-29 20:25:44 +0700

    Add resize for all cols

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,15 +3,15 @@ const CODES = {
   Z: 90,
 };
 
-function toCell() {
+function toCell(_, col) {
   return `
-    <div class="cell" contenteditable></div>
+    <div class="cell" contenteditable data-col="${col}"></div>
   `;
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
   return `
-    <div class="column" data-type="resizeable">
+    <div class="column" data-type="resizeable" data-col="${index}">
         ${col}
         <div class="col-resize" data-resize="col"></div>
     </div>
@@ -41,12 +41,19 @@ export function createTable(rowsCount = 15) {
   const colsCount = CODES.Z - CODES.A + 1;
   const rows = [];
 
-  const cols = new Array(colsCount).fill('').map(toChar).map(toColumn).join('');
+  const cols = new Array(colsCount)
+      .fill('')
+      .map(toChar)
+      .map(toColumn)
+      .join('');
 
   rows.push(createRow(null, cols));
 
   for (let i = 0; i < rowsCount; i++) {
-    const cells = new Array(colsCount).fill('').map(toCell).join('');
+    const cells = new Array(colsCount)
+        .fill('')
+        .map(toCell)
+        .join('');
     rows.push(createRow(i + 1, cells));
   }
 
