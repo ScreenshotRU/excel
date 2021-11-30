78fd78d80c8adcf2b390949b53281aa671e6093f
ScreenshotRU <screenshot@inbox.ru>
2021-11-30 23:36:33 +0700

    Finish base table logic

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,10 +3,18 @@ const CODES = {
   Z: 90,
 };
 
-function toCell(_, col) {
-  return `
-    <div class="cell" contenteditable data-col="${col}"></div>
-  `;
+function toCell(row) {
+  return function(_, col) {
+    return `
+    <div 
+      class="cell" 
+      contenteditable 
+      data-col="${col}"
+      data-type="cell"
+      data-id="${row}:${col}"
+    ></div>
+   `;
+  };
 }
 
 function toColumn(col, index) {
@@ -41,20 +49,13 @@ export function createTable(rowsCount = 15) {
   const colsCount = CODES.Z - CODES.A + 1;
   const rows = [];
 
-  const cols = new Array(colsCount)
-      .fill('')
-      .map(toChar)
-      .map(toColumn)
-      .join('');
+  const cols = new Array(colsCount).fill('').map(toChar).map(toColumn).join('');
 
   rows.push(createRow(null, cols));
 
-  for (let i = 0; i < rowsCount; i++) {
-    const cells = new Array(colsCount)
-        .fill('')
-        .map(toCell)
-        .join('');
-    rows.push(createRow(i + 1, cells));
+  for (let row = 0; row < rowsCount; row++) {
+    const cells = new Array(colsCount).fill('').map(toCell(row)).join('');
+    rows.push(createRow(row + 1, cells));
   }
 
   return rows.join('');
